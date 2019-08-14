0d3acdcf6947568e6a0442ffc951181af26a9ad5
Patrik Drean <patrikdrean@gmail.com>
2019-08-14 13:01:33 -0600

    Moved store logic to App.js

--- TodoApp/ClientApp/src/componets/App.js
+++ TodoApp/ClientApp/src/componets/App.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import '../styles/App.css';
 import TaskList from './TaskList';
 
 const appStyle = {
-  padding: '20px',
-  backgroundColor: '#171717',
-  height: '100vh'
+    padding: '20px',
+    backgroundColor: '#171717',
+    height: '100vh'
 };
 
+const initialState = {
+    tasks: [{ id: 1, description: 'Hardcoded Value' }]
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_TASK':
+            return {
+                tasks: []
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App" style={appStyle}>
-        <TaskList />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="App" style={appStyle}>
+                    <TaskList />
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- TodoApp/ClientApp/src/componets/TaskList.js
+++ TodoApp/ClientApp/src/componets/TaskList.js
@@ -1,81 +1,67 @@
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
 import Task from './Task';
 import Form from './Form';
 
-const initialState = {
-  tasks: []
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'ADD_TASK':
-      return {
-        tasks: ['hi']
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-store.dispatch({
-  type: 'ADD_TASK',
-  task: { id: 8, description: 'from store' }
-});
 
 class TaskList extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      tasks: []
+        this.state = {
+            tasks: []
+        };
+
+        fetch('api/Tasks')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ tasks: data, loading: false });
+            });
+    }
+    
+    deleteTask = id => {
+        let updatedTasks = this.state.tasks.filter(task => {
+            return task.id !== id;
+        });
+
+        fetch(`api/Tasks/${id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        this.setState({ tasks: updatedTasks });
     };
 
-    fetch('api/Tasks')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ tasks: data, loading: false });
-      });
-  }
-  deleteTask = id => {
-    let updatedTasks = this.state.tasks.filter(task => {
-      return task.id !== id;
-    });
+    addTask = taskDescription => {
+        let tasks = this.state.tasks;
+        let newTask = { id: tasks.length + 1, description: taskDescription };
+        this.setState({ tasks: [...tasks, newTask] });
 
-    fetch(`api/Tasks/${id}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' }
-    });
+        fetch(`api/Tasks/`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newTask)
+        });
+    };
 
-    this.setState({ tasks: updatedTasks });
-  };
+    render() {
+        let taskComponentList = this.props.tasks.map(task => (
+            <Task {...task} key={task.id} deleteTask={this.deleteTask} />
+        ));
 
-  addTask = taskDescription => {
-    let tasks = this.state.tasks;
-    let newTask = { id: tasks.length + 1, description: taskDescription };
-    this.setState({ tasks: [...tasks, newTask] });
-
-    fetch(`api/Tasks/`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newTask)
-    });
-  };
-
-  render() {
-    let taskComponentList = this.state.tasks.map(task => (
-      <Task {...task} key={task.id} deleteTask={this.deleteTask} />
-    ));
-
-    return (
-      <Provider store={store}>
-        {taskComponentList}
-        <Form addTask={this.addTask} />
-      </Provider>
-    );
-  }
+        return (
+            <React.Fragment>
+                {taskComponentList}
+                <Form addTask={this.addTask} />
+            </React.Fragment>
+        );
+    }
 }
 
-export default TaskList;
+function mapStateToProps(state) {
+    return {
+        tasks: state.tasks
+    };
+}
+
+export default connect(mapStateToProps)(TaskList);
